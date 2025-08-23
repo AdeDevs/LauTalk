663066f5046cc574ade49ffb8c877568086b0674
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-08-23 22:11:43 +0100

    Enhance login page layout and styling with new sign-up form

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
 export default function LogIn() {
     return (
-        <h1>log in page</h1>
+        <div className="sign-in">
+            <div className="sign-in-bg">
+                <h1>Access <br /> your <span className="colored">LauTalk </span> account</h1>
+                <img className="girl" src="/assets/girl.png" alt="image" />
+                <img className="crsr" src="/assets/cursor.png" alt="image" />
+            </div>
+            <div className="sign-in-txt">
+                <h1>Sign Up</h1>
+                <form>
+                    <input type="text" placeholder="Full Name"/>
+                    <input type="email" placeholder="School Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <p><Link className="fgt" href="#">Forgot Password?</Link></p>
+                    <button>Log In</button>
+                    <p>Don't have an account? <Link href="/auth/signup">Sign Up Now!</Link></p>
+                </form>
+            </div>
+        </div>
     )
 }
