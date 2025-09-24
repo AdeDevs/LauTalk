91b98d73c2544dd41096b76f381c6a81af28eeb8
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2025-09-24 17:37:56 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your project structure
+    "./app/**/*.{js,ts,jsx,tsx}", // adjust based on your project structure
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
